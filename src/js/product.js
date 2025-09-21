60132594a83a9217c1b70ff6b12cffe09701fdd1
meisakae <sakameeeeii17@gmail.com>
2025-09-21 00:32:08 -0600

    week03 individual activity

--- src/js/product.js
+++ src/js/product.js
@@ -9,27 +9,3 @@ const productId = getParam('product');
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
-
-//function addProductToCart(product) {
-//  var cart = getLocalStorage("so-cart") || [];
-  //cart.push(product);
-  //console.log("Item Added to cart successfully");
-//  setLocalStorage("so-cart", cart);
-//}
-
-function addProductToCart(product) {
-  const cart = getLocalStorage("so-cart") || [];
-  cart.push(product);
-  setLocalStorage("so-cart", cart);
-}
-
-// add to cart button event handler
-//async function addToCartHandler(e) {
-//  const product = await dataSource.findProductById(e.target.dataset.id);
-//  addProductToCart(product);
-//}
-
-// add listener to Add to Cart button
-//document
-//  .getElementById("addToCart")
-//  .addEventListener("click", addToCartHandler);
